e78d27bc0e21b8af2ddeae2c6a55f0f694daae83
Robin <robinslot@hotmail.nl>
2016-03-08 21:04:24 +0100

    Improved code looks :)

--- src/net/breecko/eersteplugin/HashMaps.java
+++ src/net/breecko/eersteplugin/HashMaps.java
@@ -8,7 +8,9 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public class HashMaps {
+	
 	public static Map<UUID,Boolean> AFK = new HashMap<UUID,Boolean>();
+	
 	public static void addPlayersToMaps(){
 		for(Player player: Bukkit.getOnlinePlayers()){
 			AFK.put(player.getUniqueId(), false);
